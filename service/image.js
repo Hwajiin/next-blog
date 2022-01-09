8f7a48ab08661239c926647bb0706b7a022a4944
Hwajiin <borderline0px@gmail.com>
2022-01-09 18:49:06 +0900

    ✨ Make a upload function in ImageSercive class

---
+++ service/image.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+class ImageService {
+  constructor() {
+    this.cloudinary_baseURL = "https://api.cloudinary.com/v1_1/djzpo9g8p";
+    this.cloudinary_instance = axios.create({
+      baseURL: this.cloudinary_baseURL,
+    });
+  }
+
+  upload = async (imgFile) => {
+    const formData = new FormData();
+    formData.append("file", imgFile);
+    const res = await this.cloudinary_instance.post("/image/upload", formData, {
+      params: {
+        upload_preset: "yu703dzk",
+      },
+    });
+
+    return await res.data;
+  };
+}
+
+export default ImageService;
