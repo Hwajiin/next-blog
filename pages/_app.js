49509bfdcdfabc1511b089ad09df48e3853c9bd7
Hwajiin <borderline0px@gmail.com>
2022-01-09 18:45:14 +0900

    🎉 Set up for the project

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Layout from "../components/layout";
+import "../styles/globals.css";
+import ImageService from "../service/image";
+import Database from "../service/database";
+import { useRouter } from "next/router";
+
+const imageService = new ImageService();
+const database = new Database();
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  return (
+    <Layout>
+      <Component
+        imageService={imageService}
+        database={database}
+        {...pageProps}
+      />
+    </Layout>
+  );
 }
 
-export default MyApp
+export default MyApp;
