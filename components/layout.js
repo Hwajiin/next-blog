bb4fe0b938482d9a0ef003fef401e4eb909d2261
Hwajiin <borderline0px@gmail.com>
2022-01-12 22:43:07 +0900

    ⚰️ Remove dead code about id of auth

--- components/layout.js
+++ components/layout.js
@@ -1,20 +1,12 @@
-import { useState } from "react";
 import Footer from "./footer";
 import Header from "./header";
 
 export default function Layout({ children }) {
-  let id;
-  if (children.props.uid) {
-    id = children.props.uid;
-  } else {
-    id = null;
-  }
-
   return (
     <div>
       <Header />
       {children}
-      <Footer id={id} />
+      <Footer />
     </div>
   );
 }
