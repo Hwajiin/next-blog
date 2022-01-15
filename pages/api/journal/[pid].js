2e1fe459b38dde2f31d80090776be42544ea6344
Hwajiin <borderline0px@gmail.com>
2022-01-15 23:22:34 +0900

    ✨ Create FAQ, gallery, journal api for each post

---
+++ pages/api/journal/[pid].js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  let response;
+  try {
+    switch (req.method) {
+      case "DELETE":
+        response = await axios.delete(
+          `${process.env.NEXT_PUBLIC_FIREBASE_APP_DB_URL}/journal/${req.query.pid}.json`,
+          {
+            params: {
+              auth: req.query.auth,
+            },
+          }
+        );
+        return response;
+
+      case "PATCH":
+        response = await axios.patch(
+          `${process.env.NEXT_PUBLIC_FIREBASE_APP_DB_URL}/journal/${req.query.pid}.json`,
+          req.body,
+          {
+            params: {
+              auth: req.query.auth,
+            },
+          }
+        );
+        return response;
+      default:
+        return;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
