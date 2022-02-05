51588b645e270a19cc79c2436fd90aa22e189a55
Hwajiin <borderline0px@gmail.com>
2022-02-05 22:37:18 +0900

    🚚 Move files to each directory

--- module/button.js
+++
@@ -1,3 +0,0 @@
-export default function Button({ name = "button" }) {
-  return <button>{name}</button>;
-}
--- module/link.js
+++
@@ -1,9 +0,0 @@
-import Link from "next/link";
-
-export default function CustomLink({ path = "/", name = "link" }) {
-  return (
-    <Link href={path}>
-      <a>{name}</a>
-    </Link>
-  );
-}
