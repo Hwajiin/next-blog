7769818f93044c340488a6cd014520c40227aea6
Hwajiin <borderline0px@gmail.com>
2022-02-06 20:41:25 +0900

    ✨ Create and style a  postItem component

---
+++ components/post/postItem/postItem.js
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./postItem.module.scss";
+
+export default function PostItem({
+  path,
+  image = null,
+  title,
+  contents = null,
+}) {
+  const preview = contents?.replaceAll(/(<br>|<br \/>|<br\/>)/g, "\r\n");
+
+  return (
+    <li className={styles.item}>
+      <Link href={path}>
+        <a className={styles.link}>
+          {image && (
+            <div className={styles.image}>
+              <Image src={image} layout="fill" objectFit="cover" alt={title} />
+            </div>
+          )}
+
+          <div className={styles.contentsBox}>
+            <h3 className={styles.title}>{title}</h3>
+            {preview && (
+              <p className={styles.preview}>
+                {preview.length > 50 ? `${preview.slice(0, 50)}...` : preview}
+              </p>
+            )}
+          </div>
+        </a>
+      </Link>
+    </li>
+  );
+}
