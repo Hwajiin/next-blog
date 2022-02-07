f8762248d37f887abeb234f8e3300f9b299ade9c
Hwajiin <borderline0px@gmail.com>
2022-02-07 23:24:53 +0900

    ✨ Create and style a fileInput component

---
+++ components/fileInput/fileInput.js
@@ -0,0 +1,29 @@
+import { useForm } from "../form/form";
+import styles from "./fileInput.module.scss";
+
+export default function FileInput({ label, id, name }) {
+  const { form, formChangeHandler } = useForm();
+
+  console.log(form);
+
+  return (
+    <div className={styles.fileInput}>
+      <label htmlFor={id}>
+        {label}
+        <div className={styles.state}>
+          {form.imgFile ? form.imgFile.name : "파일을 업로드해주세요 🐰"}
+        </div>
+      </label>
+
+      <input
+        id={id}
+        type="file"
+        accept="image/*"
+        name={name}
+        onChange={formChangeHandler}
+      />
+
+      <button></button>
+    </div>
+  );
+}
