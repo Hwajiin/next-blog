823cfcba34fbc0d959814023651b0842796d9bf7
Hwajiin <borderline0px@gmail.com>
2022-02-10 18:59:55 +0900

    🐛 Remove arg from router.reload method

--- components/post/postDetail/postDetail.js
+++ components/post/postDetail/postDetail.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { useState } from "react";
 import { useAuth } from "../../../context/auth";
 import { useDB } from "../../../context/database";
 import { useImageService } from "../../../context/image";
@@ -35,7 +36,7 @@ export default function PostDetail({ category, data, isAuth, pid }) {
     } catch (error) {
       console.log(error);
     } finally {
-      router.reload(router.asPath);
+      router.reload();
     }
   };
 
